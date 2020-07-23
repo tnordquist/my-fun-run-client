1a75bc4487e2c22833b63266772ba880b8f7465f
Christie Ryan <cryan11@cnm.edu>
2020-07-23 11:06:40 -0600

    Reviewed work with Todd

--- app/src/main/java/edu/cnm/deepdive/myfunrun/controller/HomeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/myfunrun/controller/HomeFragment.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive.myfunrun.controller;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -19,10 +20,13 @@ public class HomeFragment extends Fragment {
   ) {
     // Inflate the layout for this fragment
    View root = inflater.inflate(R.layout.fragment_home, container, false);
-   root.findViewById(R.id.navigate_to_history).setOnClickListener((v) -> {
-     NavHostFragment.findNavController(this)
-         .navigate(R.id.action_home_fragment_to_history_fragment);
+   root.findViewById(R.id.navigate_to_history).setOnClickListener(new OnClickListener() {
+     @Override
+     public void onClick(View v) {
+       NavHostFragment.findNavController(HomeFragment.this)
+           .navigate(R.id.action_home_fragment_to_history_fragment);
 
+     }
    });
     return root;
   }
