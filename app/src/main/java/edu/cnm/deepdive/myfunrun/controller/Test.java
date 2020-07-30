09112fd8538a72787e8f50b60268d295eb7b084e
Jason Shaner <jshaner@cnm.edu>
2020-07-30 09:29:44 -0600

    test

--- app/src/main/java/edu/cnm/deepdive/myfunrun/controller/Test.java
+++ app/src/main/java/edu/cnm/deepdive/myfunrun/controller/Test.java
@@ -3,7 +3,7 @@ package edu.cnm.deepdive.myfunrun.controller;
 public class Test {
 
   public static void main(String[] args) {
-    System.out.println("Hello World In Quotes");
+    System.out.println("Goodbye World In Quotes");
   }
 
 }
